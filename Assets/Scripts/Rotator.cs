173b84c729e24c7288f358f0dcccc51abd905128
philoniare <philoniare@gmail.com>
2016-02-11 15:52:04 -0500

    Initial commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotation = new Vector3(15, 30, 45);
+        transform.Rotate(rotation * Time.deltaTime);	
+	}
+}
